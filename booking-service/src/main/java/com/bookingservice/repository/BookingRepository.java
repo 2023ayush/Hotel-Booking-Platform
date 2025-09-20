7ad4ee304ca46a25ff64586e7db4c3cca91d7dca
Ayush Guragain <138192019+2023ayush@users.noreply.github.com>
2025-09-20 16:48:07 +0530

    Update BookingRepository.java

--- booking-service/src/main/java/com/bookingservice/repository/BookingRepository.java
+++ booking-service/src/main/java/com/bookingservice/repository/BookingRepository.java
@@ -4,5 +4,10 @@ import com.bookingservice.entity.Bookings;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Bookings, Long> {
-
+    // @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END " +
+    //        "FROM HotelBooking b " +
+    //        "WHERE b.roomId = :roomId AND b.checkInDate < :newCheckOut AND b.checkOutDate > :newCheckIn")
+    // boolean existsOverlappingBooking(@Param("roomId") Long roomId,
+    //                                  @Param("newCheckIn") LocalDate newCheckIn,
+    //                                  @Param("newCheckOut") LocalDate newCheckOut);
 }
