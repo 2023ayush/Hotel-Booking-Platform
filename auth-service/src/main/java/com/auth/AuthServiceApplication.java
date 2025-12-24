289c999a160ab2ba5aa1e475c833ff45b833ea0d
Your GitHub Username <guragain.aayush7@gmail.com>
2025-12-24 10:56:48 +0530

    Update service applications and pom files with new features

--- auth-service/src/main/java/com/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/auth/AuthServiceApplication.java
@@ -1,5 +1,6 @@
 package com.auth;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +10,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("AUTH_DB_URL", dotenv.get("AUTH_DB_URL"));
+		System.setProperty("AUTH_DB_USERNAME", dotenv.get("AUTH_DB_USERNAME"));
+		System.setProperty("AUTH_DB_PASSWORD", dotenv.get("AUTH_DB_PASSWORD"));
+		System.setProperty("EUREKA_SERVER", dotenv.get("EUREKA_SERVER"));
+
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
-
 }
--- booking-service/src/main/java/com/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/bookingservice/BookingServiceApplication.java
@@ -1,5 +1,6 @@
 package com.bookingservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,7 +10,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("BOOKING_DB_URL", dotenv.get("BOOKING_DB_URL"));
+		System.setProperty("BOOKING_DB_USERNAME", dotenv.get("BOOKING_DB_USERNAME"));
+		System.setProperty("BOOKING_DB_PASSWORD", dotenv.get("BOOKING_DB_PASSWORD"));
+		System.setProperty("EUREKA_SERVER", dotenv.get("EUREKA_SERVER"));
+
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
-
 }
