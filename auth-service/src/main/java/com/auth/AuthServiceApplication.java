e508991b5ce520e41e078f5827407a0dcd63606b
Your GitHub Username <guragain.aayush7@gmail.com>
2025-12-28 19:37:23 +0530

    feat(auth-service): docker support and config server integration

--- auth-service/src/main/java/com/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/auth/AuthServiceApplication.java
@@ -10,12 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("AUTH_DB_URL", dotenv.get("AUTH_DB_URL"));
-		System.setProperty("AUTH_DB_USERNAME", dotenv.get("AUTH_DB_USERNAME"));
-		System.setProperty("AUTH_DB_PASSWORD", dotenv.get("AUTH_DB_PASSWORD"));
-		System.setProperty("EUREKA_SERVER", dotenv.get("EUREKA_SERVER"));
-
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 }
